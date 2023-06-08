33bb544ab676d606e097cc88011491e0559c5fd6
molee1354 <molee1354@gmail.com>
2023-06-08 15:14:10 +0900

    python formatting edits

--- Day3/Python/main.py
+++ Day3/Python/main.py
@@ -4,9 +4,9 @@ import rucksack
 
 
 def main() -> None:
-  ruck = rucksack.Ruck(sys.argv[1])
-  print(f"[Part 1] Answer : {ruck.part1()}")
-  print(f"[Part 2] Answer : {ruck.part2()}")
+    ruck = rucksack.Ruck(sys.argv[1])
+    print(f"[Part 1] Answer : {ruck.part1()}")
+    print(f"[Part 2] Answer : {ruck.part2()}")
 
 
 if __name__ == "__main__":
